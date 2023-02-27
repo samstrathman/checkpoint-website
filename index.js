9cb8a00fe2f7f9576a1e085d1cdaf2d8c1db2bc2
samstrathman <117926965+samstrathman@users.noreply.github.com>
2023-02-27 15:25:34 -0500

    Added loading screen

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-//This will eventually be for a loading screen
-document.addEventListener('readystatechange', function(ev) {
-    console.log(document.readyState)
-    if((document.readyState) === 'complete') {
-        let page = document.getElementById('body');
-        console.log(page);
-    }
-});
-
-//Dynamically change the theme of the website, including video, background color, and font color
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+//Data for various site themes
 let checkpointVids = [
     {color : '#8b81f2', text : 'Assets/checkpointTextPurple.png', videoWebm : 'Assets/AAIsland_VP9.webm', videoMov : 'Assets/AAIsland_HEVC.mov', fontColor : "#6658f0", spotifyFrame : "https://open.spotify.com/embed/artist/0ChqHQDUwXWdJZVIc7ZXjq?utm_source=generator&theme=0"}, 
     {color : '#ec6272', text : 'Assets/checkpointTextPink.png', videoWebm : 'Assets/EIsland_VP9.webm', videoMov : 'Assets/EIsland_HEVC.mov', fontColor : "#ed8893", spotifyFrame : "https://open.spotify.com/embed/track/4a8SQCvS8pydJ3hpauvm4v?utm_source=generator&theme=0"}, 
@@ -20,6 +7,23 @@ let checkpointVids = [
     {color : '#6cb0d1', text : 'Assets/checkpointTextLtBlue.png', videoWebm : 'Assets/BSIsland_VP9.webm', videoMov : 'Assets/BSIsland_HEVC.mov', fontColor : "#288cbd", spotifyFrame : "https://open.spotify.com/embed/track/6fC7WAqlEbZ3qTsX8lk7Hr?utm_source=generator&theme=0"},
     {color : '#e1ab5b', text : 'Assets/checkpointTextYellow.png', videoWebm : 'Assets/DSIsland_VP9.webm', videoMov : 'Assets/DSIsland_HEVC.mov', fontColor : "#f1c84d", spotifyFrame : "https://open.spotify.com/embed/track/4wi3nxomivvjyx1qQvJCqx?utm_source=generator&theme=0"}
 ];
+
+//Code for loading screen
+function loadAnimation(){
+    setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("site-wrapper").style.display = "block";
+}
+
+//Dynamically change the theme of the website, including video, background color, and font color
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 window.addEventListener('load', () => {
     let num = Math.floor(Math.random() * 6);
     document.body.style.backgroundColor = checkpointVids[num].color;
